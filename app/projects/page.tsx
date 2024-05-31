26dfaa0fa7fba9ff09d315ef06ab8f7fc1d9b76d
Joel Thaduri <joel19thaduri@gmail.com>
2024-06-01 00:39:13 +0530

    update page.tsx, added new files

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            you thinking why there's projects in a blog? yeah me too
+            you must be thinking why there's a project section in a blog right? yeah me too
           </p>
         </div>
         <div className="container py-12">
